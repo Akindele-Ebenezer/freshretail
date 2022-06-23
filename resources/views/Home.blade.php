b696c3258226c9636c074592bfa3fcecc5bcc7bf
Akindele Ebenezer <tobi.akindele@gmail.com>
2022-06-23 14:00:53 +0100

    Fresh

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.layout-1')
+
+@section('content')
+    <section class="sec-1" style="background-image: url('/images/bg.jpg')">
+        <div class="sec-1-inner">
+            <div>
+                <span>Networking Solutions..</span>
+                <h1>FRESH RETAIL</h1>
+                <h2>HUGE SALE <br> UP TO 70% OFF</h2><br>
+                <button>Quick View</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="sec-2">
+        <div class="sec-2-inner">
+            <h1>Browse the latest collections.</h1> 
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi tenetur eum ex, ea recusandae delectus maiores inventore quisquam mollitia earum.</p>
+            <p>Buy from FRESH RETAIL today!</p>
+        </div>
+        <div class="sec-2-inner">
+            <div style="background-image: url('/images/switch.webp')"></div>
+            <p>Cable collection <em>7</em></p>
+        </div>
+        <div class="sec-2-inner">
+            <div style="background-image: url('/images/switch.webp')"></div>
+            <p>Computer collection <em>4</em></p>
+        </div>
+        <div class="sec-2-inner">
+            <div style="background-image: url('/images/switch.webp')"></div>
+            <p>IP Phones <em>2</em></p>
+        </div>
+    </section>
+
+    <div class="products-title-wrapper">
+        <div class="products-title"> 
+            <hr> 
+            <span>MOST DEMANDED</span>
+        </div>
+    </div>
+
+    <div class="products">
+        <div class="products-inner">
+            <div class="product">
+                <div style="background-image: url('/images/switch.webp')"></div>
+                <span>TP-Link</span>
+                <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                <h1>₦35,000</h1> 
+            </div>
+            <div class="product">
+                <div style="background-image: url('/images/switch.webp')"></div>
+                <span>TP-Link</span>
+                <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                <h1>₦35,000</h1> 
+            </div>
+            <div class="product">
+                <div style="background-image: url('/images/switch.webp')"></div>
+                <span>TP-Link</span>
+                <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                <h1>₦35,000</h1> 
+            </div>
+            <div class="product">
+                <div style="background-image: url('/images/switch.webp')"></div>
+                <span>TP-Link</span>
+                <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                <h1>₦35,000</h1> 
+            </div>
+            <div class="product">
+                <div style="background-image: url('/images/switch.webp')"></div>
+                <span>TP-Link</span>
+                <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                <h1>₦35,000</h1> 
+            </div>
+            <div class="product">
+                <div style="background-image: url('/images/switch.webp')"></div>
+                <span>TP-Link</span>
+                <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                <h1>₦35,000</h1> 
+            </div>
+            <div class="product">
+                <div style="background-image: url('/images/switch.webp')"></div>
+                <span>TP-Link</span>
+                <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                <h1>₦35,000</h1> 
+            </div>
+        </div>
+    </div>
+@endsection
+
+<!-- https://preview.themeforest.net/item/flatsome-multipurpose-responsive-woocommerce-theme/full_screen_preview/5484319?irgwc=1&clickid=1mvX%3AmWhHxyIUeizap0BSzDkUkDzBF2b1RlDxU0&iradid=275988&irpid=369282&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_369282&utm_medium=affiliate&utm_source=impact_radius -->
+
+<!-- https://avada.theme-fusion.com/retail/ -->
