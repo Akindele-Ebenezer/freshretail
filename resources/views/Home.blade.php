ab106023ace6bfdc4f0c2508494cd9b2acd128a7
Akindele Ebenezer <tobi.akindele@gmail.com>
2022-07-01 09:51:47 +0100

    First Page

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -15,14 +15,116 @@
     
     <main>
         <div class="main">
+            
+            <div class="products-title-wrapper">
+                <div class="products-title">  
+                    <span>MOST DEMANDED</span>
+                </div>
+            </div>
+            
+            <div class="products">
+                <div class="products-inner">
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                    <div class="product">
+                        <h1 class="sale">SALE</h1>
+                        <div style="background-image: url('/images/switch.webp')"></div>
+                        <span>TP-Link</span>
+                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
+                        <h1>₦35,000</h1> 
+                    </div>
+                </div>
+            </div>
+            
             <section class="sec-2">
                 <div class="sec-2-inner">
                     <h1>Browse the latest collections.</h1> 
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi tenetur eum ex, ea recusandae delectus maiores inventore quisquam mollitia earum.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi tenetur eum ex, ea recusandae delectus maiores inventore quisquam mollitia earum. <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cupiditate, labore tenetur saepe ullam, numquam culpa unde, officiis obcaecati aperiam suscipit minus delectus fugit hic architecto! Cumque aperiam adipisci hic, sit perferendis, dolores illo magni dolorum eos quod laboriosam corporis?</p>
                     <p>Buy from FRESH RETAIL today!</p>
                 </div>
                 <div class="sec-2-inner">
-                    <h1>SALE</h1>
                     <div style="background-image: url('/images/cables.webp')">
                         <div>
                             <h2>CABLES</h2>
@@ -48,63 +150,38 @@
                 </div>
             </section>
 
-            <div class="products-title-wrapper">
-                <div class="products-title"> 
-                    <hr> 
-                    <span>MOST DEMANDED</span>
+            <section class="sec-3" style="background-image: url('/images/bg.jpg')">
+                <div>
+                    <form action="">
+                        <h1>Subscribe to our newsletter and get 10% Off!</h1>
+                        <p>Velit morbi sed non est dolor tellus fringilla diam</p>
+                        <br>
+                        <input type="email" name=""> <button>Subscribe</button> 
+                    </form>
                 </div>
-            </div>
+            </section>
 
-            <div class="products">
-                <div class="products-inner">
-                    <div class="product">
-                        <div style="background-image: url('/images/switch.webp')"></div>
-                        <span>TP-Link</span>
-                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
-                        <h1>₦35,000</h1> 
-                    </div>
-                    <div class="product">
-                        <div style="background-image: url('/images/switch.webp')"></div>
-                        <span>TP-Link</span>
-                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
-                        <h1>₦35,000</h1> 
-                    </div>
-                    <div class="product">
-                        <div style="background-image: url('/images/switch.webp')"></div>
-                        <span>TP-Link</span>
-                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
-                        <h1>₦35,000</h1> 
-                    </div>
-                    <div class="product">
-                        <div style="background-image: url('/images/switch.webp')"></div>
-                        <span>TP-Link</span>
-                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
-                        <h1>₦35,000</h1> 
-                    </div>
-                    <div class="product">
-                        <div style="background-image: url('/images/switch.webp')"></div>
-                        <span>TP-Link</span>
-                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
-                        <h1>₦35,000</h1> 
-                    </div>
-                    <div class="product">
-                        <div style="background-image: url('/images/switch.webp')"></div>
-                        <span>TP-Link</span>
-                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
-                        <h1>₦35,000</h1> 
-                    </div>
-                    <div class="product">
-                        <div style="background-image: url('/images/switch.webp')"></div>
-                        <span>TP-Link</span>
-                        <p>TP-Link 24 Port 10-100 Rackmount Switch</p>
-                        <h1>₦35,000</h1> 
-                    </div>
+            <section class="sec-4">
+                <div>
+                    <center><img src="/images/buy.png"></center>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, facere?.Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, facere?</p>
+                    <span>Buy Fresh Retail Products today!</span>
                 </div>
-            </div>
+                <div>
+                    <center><img src="/images/collection.png"></center>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, facere?.Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, facere?</p>
+                    <span>View latest collections</span>
+                </div>
+                <div>
+                    <center><img src="/images/contact-us.png"></center>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, facere?.Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, facere?</p>
+                    <span>Contact Us</span>
+                </div>
+            </section>
         </div>
     </main>
-@endsection
-
-<!-- https://preview.themeforest.net/item/flatsome-multipurpose-responsive-woocommerce-theme/full_screen_preview/5484319?irgwc=1&clickid=1mvX%3AmWhHxyIUeizap0BSzDkUkDzBF2b1RlDxU0&iradid=275988&irpid=369282&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_369282&utm_medium=affiliate&utm_source=impact_radius -->
-
+    @endsection
+    
+    <!-- https://preview.themeforest.net/item/flatsome-multipurpose-responsive-woocommerce-theme/full_screen_preview/5484319?irgwc=1&clickid=1mvX%3AmWhHxyIUeizap0BSzDkUkDzBF2b1RlDxU0&iradid=275988&irpid=369282&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_369282&utm_medium=affiliate&utm_source=impact_radius -->
+    
 <!-- https://avada.theme-fusion.com/retail/ -->
